b5476ac14981818ae399d1dcb259884b8e5077f3
qiaoyafeng <qiaoyafeng@gmail.com>
2024-08-06 19:41:38 +0800

    update start script

--- basic_demo/start.sh
+++ basic_demo/start.sh
@@ -2,4 +2,18 @@
 # 激活虚拟环境
 conda activate glm4
 
-nohup python  trans_web_demo.py  1>&2 > chat.log  & 
+
+pid=$(ps aux | grep trans_web_demo.py  | grep -v grep | awk '{print $2}')
+echo "pid:$pid"
+
+if [ -n "$pid" ];then
+          echo "kill pid:$pid"
+            kill -9  $pid
+fi
+( nohup python  trans_web_demo.py  1>&2 > chat.log  & )
+
+
+
+
+
+
